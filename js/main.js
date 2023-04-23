200a70ad616dd150c20416065f1a1d9f4c99b923
Ahmetqaz <107630795+Ahmetqaz@users.noreply.github.com>
2023-04-23 20:44:45 +0500

    7

--- js/main.js
+++ js/main.js
@@ -81,3 +81,15 @@ window.onclick = function (event) {
     closeMenu();
   }
 };
+
+// wow start
+var wow = new WOW({
+  boxClass: "wow",
+  animateClass: "animated",
+  offset: 0,
+  mobile: true,
+  live: true,
+  scrollContainer: null,
+  resetAnimation: true,
+});
+wow.init();
